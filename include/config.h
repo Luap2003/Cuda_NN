756f3e3cc572a37beca19be0a4ac939a5876e194
Wiener234 <wiemernils@gmail.com>
2025-01-27 20:05:25 +0100

    add missing params to config.c

--- include/config.h
+++ include/config.h
@@ -4,13 +4,31 @@
 
 #include "activations.h"
 
-typedef struct Config{
-  int *hidden_layers;
-  int input_layer;
-  int output_layer;
-  int number_layers;
-  ActivationType *activation_functions;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Define the Config struct
+typedef struct Config {
+    int *hidden_layers;
+    int input_layer;
+    int output_layer;
+    int number_layers;
+    int *layers_sizes;
+    ActivationType *activation_functions;
+
+    int batch_size;
+    int num_epochs;
+    float learning_rate;
+    float decay_rate;
 } Config;
 
+// Declare the functions you want to use in C and C++ files
+void remove_spaces(char* s);
 int parser(char *config_file, Config *config);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif // CONFIG_H
--- src/config.c
+++ src/config.c
@@ -49,10 +49,6 @@ int parse_config(char *config_file, Config *config){
       config->output_layer = atoi(value);
 
 
-    
-
-    if(strcmp(key, "hidden_layers") == 0){
-      
       int count = 0;
       int size = 1;
       for (const char *p = value; *p; p++) {
@@ -67,6 +63,7 @@ int parse_config(char *config_file, Config *config){
         free(config->hidden_layers);
         printf("Memory allocatoins failed! \n");
         return -1;
+        return 1;
       }
 
       char *start = value + 1;
@@ -88,8 +85,6 @@ int parse_config(char *config_file, Config *config){
       
     } 
 
-    if(strcmp(key, "activation_functions") == 0){
-      
       int size = 1;
       for (const char *p = value; *p; p++) {
           if (*p == ',') {
@@ -104,6 +99,7 @@ int parse_config(char *config_file, Config *config){
       if(str_arr == NULL){
         printf("Memory allocatoins failed! \n");
         return -1;
+        return 1;
       }
 
       char *start = value + 1;
@@ -123,6 +119,7 @@ int parse_config(char *config_file, Config *config){
         if(str_arr[pos] == NULL){
           printf("Memory allocation failed!\n");
           return -1;
+          return 1;
         }
         for(int i = 0; i<len; i++){
           str_arr[pos][i] = token[i];
@@ -139,6 +136,57 @@ int parse_config(char *config_file, Config *config){
         token = strtok(NULL, ",");
       }
       
+int parser(char *config_file, Config *config){
+
+  
+  char line[256];
+  FILE *file = fopen("config.txt", "r");
+  if(file == NULL){
+    printf("Can not open file!\n");
+    return 1;
+  }
+
+  while(fgets(line, sizeof(line), file)){
+
+    char *key;
+    char *value;
+    remove_spaces(line);
+
+    
+    if(line[0] == '#')
+      continue;
+    if(line[0] == '/')
+      if(line[1] == '/')
+        continue;
+    if(line[0] == '\n')
+      continue;
+
+    
+    line[strcspn(line, "\n")] = '\0';
+    key = strtok(line, "=");
+    value = strtok(NULL, "=");
+
+    
+    if(strcmp(key, "batch_size") == 0)
+      config->batch_size = atoi(value);
+
+    if(strcmp(key, "num_epochs") == 0)
+      config->num_epochs = atoi(value);
+
+    if(strcmp(key, "learning_rate") == 0)
+      config->learning_rate = atof(value);
+
+    if(strcmp(key, "decay_rate") == 0)
+      config->decay_rate = atof(value);
+
+    if(strcmp(key, "input_layer") == 0)
+      config->input_layer = atoi(value);
+
+    if(strcmp(key, "output_layer") == 0)
+      config->output_layer = atoi(value);
+
+    if(strcmp(key, "hidden_layers") == 0){
+    if(strcmp(key, "activation_functions") == 0){
     } 
   }
 
@@ -178,6 +226,43 @@ int parse_config(char *config_file, Config *config){
   // free(config->activation_functions);
   
   
+  config->layers_sizes = (int *) malloc(config->number_layers * sizeof(int));
+  config->layers_sizes[0] = config->input_layer;
+  if(config->hidden_layers != NULL){
+    for(int i = 0; i<config->number_layers-2; i++){
+      config->layers_sizes[i+1] = config->hidden_layers[i];
+    }
+  }
+  config->layers_sizes[config->number_layers-1] = config->output_layer;
+
+
+
+  
+  #if defined(DEBUG) || defined(CONFIG_DEBUG)
+  printf("Hidden Layers:");
+  if(config->hidden_layers != NULL){
+    for(int i = 0; i < config->number_layers-2; i++){
+      printf(" %d",config->hidden_layers[i]);
+      if(i < config->number_layers -3)
+        printf(",");
+    }
+  }
+  printf("\n");
+  printf("Input Layer: %d\n", config->input_layer);
+  printf("Output Layer: %d\n", config->output_layer);
+  printf("Number of Layers: %d\n", config->number_layers);
+  printf("Activation Functions:");
+  if(config->activation_functions != NULL){
+    for(int i = 0; i < config->number_layers-1; i++){
+      printf(" %d", config->activation_functions[i]);
+      if(i < config->number_layers - 2)
+        printf(",");
+    }
+  }
+  printf("\n");
+
+  #endif
+
   return 0;
 }
 
