56d3e2a532c4d77a890e5493e19233fefeb3e23a
Wiener234 <wiemernils@gmail.com>
2025-02-24 21:14:57 +0100

    docs: complete docs for layer.h

--- include/layers.h
+++ include/layers.h
@@ -12,6 +12,12 @@
 #include "lossFunction.h"
 #include <cublas_v2.h>
 #define THREADS_PER_BLOCK 256
+/**
+ * @struct Layer
+ * @brief Representing one Layer
+ *
+ * This struct is used to store the parameters of a layer.
+ */
 typedef struct {
     int m;              // Batch size
     int n_in;           // Number of input neurons
@@ -28,7 +34,18 @@ typedef struct {
 
     ActivationType aktfunc;
 } Layer;
+/**
+ * @brief Initializes layer.
+ *
+ * Allocates memory on devices for @ref Layer struct and initializes the weights
+ * with Glorot Uniform. The bias is initialized to zero.
 
+ * @param layer Pointer to a @ref Layer struct to initialize. 
+ * @param m Batch size.
+ * @param n_in Number of input neurons.
+ * @param n_out Number of output neurons.
+ * @param aktfunc Aktivation Function from @ref ActivationType enum.
+ */
 void layer_init(Layer *layer, int m, int n_in, int n_out, ActivationType aktfunc);
 
 /**
@@ -43,11 +60,41 @@ void layer_init(Layer *layer, int m, int n_in, int n_out, ActivationType aktfunc
  */
 void layer_forward(Layer *layer, float *A_prev_d, cublasHandle_t handle);
 
+/**
+ * @brief Performs backword propagation on output layer.
+ *
+ * Calculates the gradient of the pre-activation values and the gradient of the weights and biases. 
+ *
+ * @param layer Pointer to @ref Layer struct.
+ * @param Y Pointer to true labels.
+ * @param A_prev_d Activations from the previous layer.
+ * @param handle CublasHandle.
+ */
 void backward_output_layer(Layer *layer, float *Y, float *A_prev_d, cublasHandle_t handle);
+
+/**
+ * @brief Performs backword propagation on hidden layers.
+ *
+ * Propagates gradients from previous layer and applies activation function derivative.
+ * Followed by calculating the derivative of the weights and biases.
+ *
+ * @param layer Pointer to @ref Layer struct.
+ * @param W_next_d Derivativions of the weights of the next layer.
+ * @param dZ_next_d Derivativions of the pre-activation values of the next layer.
+ * @param A_prev_d Derivations of the activations from the previous layer.
+ * @param n_out_next Number of neurons in the next layer.
+ * @param handle CublasHandle.
+ */
 void backward_layer(Layer *layer, float *W_next_d, float *dZ_next_d, float *A_prev_d, int n_out_next, cublasHandle_t handle);
 
-
+/**
+ * @brief Update weights and biases of the layers.
+ *
+ * @param layer Pointer to @ref Layer struct.
+ * @param learning_rate Learning rate.
+ */
 void update(Layer *layer, float learning_rate);
+
 /**
  * @brief Frees the memory allocated for a layer.
  *
